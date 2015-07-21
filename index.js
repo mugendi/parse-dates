345ea59b9fdf9d737aa8ad7da899345f36fe5f6e
Anthony Mugendi <ngurumugz@gmail.com>
2015-07-21 19:55:30 +0300

    Update to use date.js

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-require('sugar')
+
 var ngrams = require ( 'n-gramify' )
+//this library changes the Dates object. And that's all we need here...
 require('./lib/date.js')
 
 module.exports=function(string){
 	string=string || "there will be a meeting next friday. Are you coming? How about next week?"
 
-	console.log(string)
 	var chunk='';
 	var date=null;
+
+	//create dates object
 	var dates={
 		dates:[],
 		string:{
-			in:Object.clone(string),
+			in:string+'',
 			out:'',
-			annotated:Object.clone(string)
+			annotated:string+''
 		}
 	}
 
-
-
 	// var ngrams=[]
 	for(i=6;i>1;i--){
-
 
 		//loop thru ngrams
 		ngrams(string,i).forEach(function(grams){
@@ -29,23 +28,19 @@ module.exports=function(string){
 			date=Date.parse(chunk);
 
 			if(date){
-				date=Date.create(date).iso();
+				//return as ISOString
+				date=new Date(date).toISOString();
 				dates.dates.push(date);
 				// console.log(chunk+">>"+date)	
-				//if date, then replace string
 				dates.string.annotated=dates.string.annotated.replace(chunk,'{DATE: '+date+'}');
-				// dates.string.out=dates.string.out.replace(d.text,date);	
 				string=string.replace(chunk,date);	
 			}			
 		});
 
 	}
 
-
+	//add out value
 	dates.string.out=string;
-
-	// console.log(JSON.stringify(dates,0,4));
-	
 
 	return dates;
 

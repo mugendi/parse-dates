26920ae66c6f45711768f94218c821f5ee556a25
Anthony Mugendi <ngurumugz@gmail.com>
2017-07-29 16:34:31 +0300

    Corrected parsing bugs when parsing null

---
+++ README.md
@@ -0,0 +1,37 @@
+
+#Extract Dates from text
+
+Easy dates parser that leverages on the power of [n-gramify](https://www.npmjs.com/package/n-gramify), [datejs](http://www.datejs.com) & [dehumanize-date](https://www.npmjs.com/package/dehumanize-date) to parse dates that other parsers would fail. 
+
+The concept is simple, instead on Regex, we tokenize the string into 'reducing ngrams' & then extract dates!
+
+**datejs** does the initial parsing & then we use **dehumanize-date** to confirm parsed tokens & format them.
+
+
+```javascript
+
+var parse_date = require ( 'parse-dates' )
+
+var s=" Peter was born in 5th january 1994 will be 44 years old next week. "
+var parsed=parse_date(s)
+
+console.log(JSON.stringify(parsed,0,4));
+
+```
+
+Output:
+
+```
+
+{
+    "dates": [
+        "1994-01-05T00:00:00.000Z"
+    ],
+    "string": {
+        "in": " Peter was born in 5th january 1994 will be 44 years old next week. ",
+        "out": " Peter was born in 1994-01-05T00:00:00.000Z will be 44 years old next week. ",
+        "annotated": " Peter was born in {DATE: 1994-01-05T00:00:00.000Z} will be 44 years old next week. "
+    }
+}                                                    
+
+  ```
--- index.js
+++ index.js
@@ -1,74 +1,75 @@
-
-const ngrams = require ( 'n-gramify' )
+const ngrams = require('n-gramify')
 //this library changes the Dates object. And that's all we need here...
 require('./lib/date.js');
 const dehumanize_date = require('dehumanize-date');
 
-module.exports=function(string){
-	string=string || "there will be a meeting next friday. Are you coming? How about next week?"
+module.exports = function (string) {
+	string = string || "there will be a meeting next friday. Are you coming? How about next week?"
 
-	var chunk='';
-	var date=null,
+	var chunk = '';
+	var date = null,
 		reducted_date = null;
 
 	//create dates object
-	var dates={
-		dates:[],
-		string:{
-			in:string+'',
-			out:'',
-			annotated:string+''
+	var dates = {
+		dates: [],
+		string: { in: string + '',
+			out: '',
+			annotated: string + ''
 		}
 	}
 
 	// var ngrams=[]
-	for(i=6;i>1;i--){
+	for (i = 6; i > 1; i--) {
 
 		//loop thru ngrams
-		ngrams(string,i).forEach(function(grams){
+		ngrams(string, i).forEach(function (grams) {
 
 			// console.log(grams);
 
-			chunk=grams.join(' ');
-			date=Date.parse(chunk);
+			chunk = grams.join(' ');
+			date = Date.parse(chunk);
 
-			if(date){
+			// console.log(date);
+
+			if (date) {
 				//find parts of grams that we can safely omit
 				// var required_grams = grams.concat([]);
 
-				while(grams.length){
+				while (grams.length) {
 
 					reducted_date = dehumanize_date(grams.join(' '));
 
-					if( reducted_date ){
+					if (reducted_date) {
 						chunk = grams.join(' ');
 						grams = [];
 					}
-					
+
 					grams.shift();
-					
+
 				}
 
+				if (reducted_date) {
+					// console.log(chunk, reducted_date);
+					// console.log(required_grams, grams);
 
-				// console.log(chunk)	;
-				// console.log(required_grams, grams);
+					// return as ISOString
+					date = new Date(reducted_date).toISOString();
+					dates.dates.push(date);
+					// console.log(chunk+">>"+date)	
+					dates.string.annotated = dates.string.annotated.replace(chunk, '{DATE: ' + date + '}');
+					string = string.replace(chunk, date);
 
-				// return as ISOString
-				date=new Date( reducted_date ).toISOString();
-				dates.dates.push(date);
-				// console.log(chunk+">>"+date)	
-				dates.string.annotated=dates.string.annotated.replace(chunk,'{DATE: '+date+'}');
-				string=string.replace(chunk,date);	
-			}			
+				}
+
+			}
 		});
 
 	}
 
 	//add out value
-	dates.string.out=string;
+	dates.string.out = string;
 
 	return dates;
 
 }
-
-
